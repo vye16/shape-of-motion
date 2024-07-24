fix depth maps saving overflow issue

flow3d/data/casual_dataset.py
@@ -230,12 +230,10 @@ class CasualDataset(BaseDataset):
         return torch.from_numpy(out_mask).float()
 
     def load_depth(self, index) -> torch.Tensor:
-        path = f"{self.depth_dir}/{self.frame_names[index]}.png"
-        disp = imageio.imread(path)
-        if disp.dtype == np.uint16:
-            disp = disp.astype(np.float32) / UINT16_MAX
+        path = f"{self.depth_dir}/{self.frame_names[index]}.npy"
+        disp = np.load(path)
         depth = 1.0 / np.clip(disp, a_min=1e-6, a_max=1e6)
-        depth = torch.from_numpy(depth)
+        depth = torch.from_numpy(depth).float()
         depth = median_filter_2d(depth[None, None], 11, 1)[0, 0]
         return depth
 

preproc/compute_depth.py
@@ -102,10 +102,12 @@ def align_monodepth_with_metric_depth(
         return
 
     for f in tqdm(img_files):
-        metric_path = osp.join(metric_depth_dir, f)
-        mono_path = osp.join(input_monodepth_dir, f)
+        imname = os.path.splitext(f)[0]
+        metric_path = osp.join(metric_depth_dir, imname + ".npy")
+        mono_path = osp.join(input_monodepth_dir, imname + ".png")
+
         mono_disp_map = iio.imread(mono_path) / UINT16_MAX
-        metric_disp_map = iio.imread(metric_path) / UINT16_MAX
+        metric_disp_map = np.load(metric_path)
         ms_colmap_disp = metric_disp_map - np.median(metric_disp_map) + 1e-8
         ms_mono_disp = mono_disp_map - np.median(mono_disp_map) + 1e-8
 
@@ -117,9 +119,8 @@ def align_monodepth_with_metric_depth(
         min_thre = min(1e-6, np.quantile(aligned_disp, 0.01))
         # set depth values that are too small to invalid (0)
         aligned_disp[aligned_disp < min_thre] = 0.0
-        aligned_disp_uint16 = (aligned_disp * UINT16_MAX).astype(np.uint16)
-        out_file = osp.join(output_monodepth_dir, f)
-        iio.imwrite(out_file, aligned_disp_uint16)
+        out_file = osp.join(output_monodepth_dir, imname + ".npy")
+        np.save(out_file, aligned_disp)
 
 
 def align_monodepth_with_colmap(

preproc/compute_metric_depth.py
@@ -31,13 +31,13 @@ def run_model_inference(img_dir: str, depth_dir: str, intrins_file: str):
     intrins_dict = {}
     for img_file in (bar := tqdm(img_files)):
         img_name = os.path.splitext(img_file)[0]
-        out_path = f"{depth_dir}/{img_name}.png"
+        out_path = f"{depth_dir}/{img_name}.npy"
         img = iio.imread(f"{img_dir}/{img_file}")
         pred_dict = run_model(model, img)
         depth = pred_dict["depth"]
         disp = 1.0 / np.clip(depth, a_min=1e-6, a_max=1e6)
         bar.set_description(f"Input {img_file} {depth.min()} {depth.max()}")
-        write_disp(out_path, disp, False)
+        np.save(out_path.replace("png", "npy"), disp.squeeze())
 
         K = pred_dict["intrinsics"]
         intrins_dict[img_name] = (
@@ -60,39 +60,6 @@ def run_model(model, rgb: np.ndarray, intrinsics: np.ndarray | None = None):
     predictions = model.infer(rgb_torch, intrinsics_torch)
     out_dict = {k: v.squeeze().cpu().numpy() for k, v in predictions.items()}
     return out_dict
-
-
-def write_disp(path, disp, rescale: bool, bits: int = 2):
-    """
-    Write disparity to png file.
-    :param path (str): filepath without extension
-    :param depth (array): depth
-    """
-    assert bits in [1, 2], "Unsupported bit depth."
-
-    if not np.isfinite(disp).all():
-        disp = np.nan_to_num(disp, nan=0.0, posinf=0.0, neginf=0.0)
-        print("WARNING: Non-finite depth values present")
-
-    max_val = (2 ** (8 * bits)) - 1
-    dtype = "uint8" if bits == 1 else "uint16"
-
-    if rescale:
-        disp_min = disp.min()
-        disp_max = disp.max()
-
-        if disp_max - disp_min > np.finfo("float").eps:
-            out = (disp - disp_min) / (disp_max - disp_min)
-        else:
-            out = np.zeros(disp.shape, dtype=disp.dtype)
-
-    out = disp * max_val
-    print(
-        f"{path} {disp.min()=}, {disp.max()=} output min: {out.min()} max: {out.max()}"
-    )
-    out = out.astype(dtype).squeeze()
-
-    iio.imwrite(path, out)
 
 
 if __name__ == "__main__":

preproc/recon_with_depth.py
@@ -79,8 +79,8 @@ def image_stream(img_dir, calib_path, stride, depth_dir: str | None = None):
         image = torch.as_tensor(image).permute(2, 0, 1)
 
         if depth_dir is not None:
-            depth_path = f"{depth_dir}/{imname}.png"
-            depth = iio.imread(depth_path) / 65535.0
+            depth_path = f"{depth_dir}/{imname}.npy"
+            depth = np.load(depth_path)
             depth, (dh0, dw0), (dh1, dw1) = preproc_image(depth, calib)
             assert dh0 == h0 and dw0 == w0 and dh1 == h1 and dw1 == w1
             depth = torch.as_tensor(depth).float()